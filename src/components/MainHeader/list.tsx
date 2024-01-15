52812ef13c9277ddb14daeebebacd71ecc3cabf6
Tannaz Shirzadi <tia.shr97@gmail.com>
2024-01-15 18:13:03 +0300

    feat: finish responsive navbar

--- src/components/MainHeader/list.tsx
+++ src/components/MainHeader/list.tsx
@@ -40,14 +40,15 @@ export default function NavbarList() {
         {isMenuOpen ? <IconCross /> : <IconMenu />}
       </button>
       <div
-        className="flex w-[90%] md:flex-1 flex-col md:flex-row absolute md:relative top-28 md:top-0 bg-bg-primary
-  border md:border-none p-4 md:p-0 h-[70vh] md:h-fit"
+        className={`${
+          isMenuOpen ? "flex" : "hidden md:flex"
+        } w-[90%] md:w-[40rem] flex-col md:flex-row absolute md:relative top-28 md:top-0 bg-bg-primary border left-1/2 transform -translate-x-1/2 md:border-none p-4 md:p-0 h-[70vh] md:h-fit`}
       >
-        <ul className="flex md:mt-auto md:ml-16 flex-col md:flex-row items-center">
+        <ul className="flex md:mt-auto flex-col md:flex-row items-baseline">
           {listItems.map((item) => (
             <li
               key={item.id}
-              className="mx-6 border-b-4 border-transparent font-semibold pb-6
+              className="mx-6 border-b-4 border-transparent font-semibold py-4
         hover:cursor-pointer hover:text-custom-orange hover:border-b-4 hover:border-custom-orange"
             >
               <Link className="capitalize" href={item.href}>
