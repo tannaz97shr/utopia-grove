14f408e8b877adfbef1658d23bdf7c8fbe0098c4
Tannaz Shirzadi <tia.shr97@gmail.com>
2024-01-21 22:49:52 +0300

    feat: fetch events from server:

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,3 +1,6 @@
-export default function Events() {
+import { fetchEvents } from "@/lib/events";
+
+export default async function Events() {
+  await fetchEvents(2, 2);
   return <div>this is events page</div>;
 }
