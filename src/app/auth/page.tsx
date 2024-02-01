ef0a1d977864b3826d9b4904c6871d3f167667c6
Tannaz Shirzadi <tia.shr97@gmail.com>
2024-02-01 15:37:07 +0300

    feat: protect auth page

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,12 +1,31 @@
+"use client";
+
 import LoginForm from "@/components/LoginForm";
 import SignupForm from "@/components/SignupForm";
 import Tabs from "@/components/Tabs";
+import { getSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export default function AuthPage({
   searchParams,
 }: {
   searchParams: { [key: string]: string | string[] | undefined };
 }) {
+  const [isLoading, setIsLoading] = useState(true);
+  const router = useRouter();
+  useEffect(() => {
+    getSession().then((session) => {
+      if (session) {
+        router.replace("/");
+      } else {
+        setIsLoading(false);
+      }
+    });
+  }, [router]);
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
   return (
     <div className="w-full lg:w-3/4 mx-auto bg-bg-secondary p-2 mt-6">
       <div className="flex flex-col border border-border-brown p-4">
