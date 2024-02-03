9d8ee8774964a450ca3a7946c9aa3c67f384565c
Tannaz Shirzadi <tia.shr97@gmail.com>
2024-02-03 20:32:37 +0300

    feat: navigate to create events

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,11 +1,21 @@
 import EventsSection from "@/components/EventsSection";
+import Button from "@/components/UI/Button";
 import { fetchEvents } from "@/lib/events";
 import { IEvent } from "@/models/general";
+import { getServerSession } from "next-auth";
 
 export default async function Events() {
   const eventsResponse: IEvent[] = await fetchEvents(1, 20);
+  const session = await getServerSession();
   return (
     <>
+      {session && (
+        <div className="mt-6">
+          <Button variant="primary" href="/events/create" className="mt-6">
+            Submit Your Event
+          </Button>
+        </div>
+      )}
       <EventsSection events={eventsResponse} />
     </>
   );
