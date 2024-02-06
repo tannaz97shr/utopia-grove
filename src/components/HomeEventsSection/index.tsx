e55edbee252afaa60386f84adb79582310ca363b
Tannaz Shirzadi <tia.shr97@gmail.com>
2024-02-06 06:00:48 +0300

    feat: create latest events section

--- src/components/HomeEventsSection/index.tsx
+++ src/components/HomeEventsSection/index.tsx
@@ -1,3 +1,23 @@
-export default function HomeEventsSection() {
-  return <div className="bg-custom-maroon">Home events</div>;
+import { fetchEvents } from "@/lib/events";
+import { IEvent } from "@/models/general";
+import PreviewCard from "./PreviewCard";
+
+export default async function HomeEventsSection() {
+  const latestEvents = await fetchEvents(1, 4);
+  console.log("latest", latestEvents);
+  if (!latestEvents.length) return;
+  return (
+    <div className="mt-8 w-full">
+      <h2 className="font-semibold text-xl w-fit mx-auto">Latest Events</h2>
+      <div className="grid gap-4 grid-cols-2 md:grid-cols-4 pt-4">
+        {latestEvents.map((event: IEvent) => (
+          <PreviewCard
+            image={event.image}
+            title={event.title}
+            link={event.link}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
