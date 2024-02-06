85943c390451c021c4fe6eb2b72be2b7b92fb362
Tannaz Shirzadi <tia.shr97@gmail.com>
2024-02-06 06:56:18 +0300

    feat: add about section

---
+++ src/components/HomeAboutSection/index.tsx
@@ -0,0 +1,51 @@
+export default function HomeAboutSection() {
+  return (
+    <div className="mt-16 w-full mb-6">
+      <h2 id="about" className="font-semibold text-2xl w-fit mx-auto">
+        About Us
+      </h2>
+      <p className="mt-6 font-sans text-lg">
+        Welcome to Dubai Events, your premier destination for experiencing and
+        creating virtual events in the dynamic city of Dubai.
+      </p>
+      <h3 className=" font-sans font-semibold text-xl mt-6">My Story</h3>
+      <p className="mt-6 font-sans text-lg">
+        As a dedicated front-end developer with a passion for innovation, I
+        created Dubai Events as a personal project to showcase my proficiency in
+        technologies like React, Next.js, and TypeScript. Inspired by the
+        dynamic city of Dubai and the endless possibilities of virtual events, I
+        set out to build a platform that demonstrates my expertise while
+        providing a glimpse into the future of online experiences.
+      </p>
+      <h3 className=" font-sans font-semibold text-xl mt-6">My Mission</h3>
+      <p className="mt-6 font-sans text-lg">
+        At Dubai Events, my mission is to push the boundaries of what's possible
+        in front-end development while creating engaging and user-friendly
+        interfaces. While this website may be a practice project, my commitment
+        to excellence and innovation shines through in every aspect of its
+        design and functionality.
+      </p>
+      <h3 className=" font-sans font-semibold text-xl mt-6">
+        What Sets Me Apart
+      </h3>
+      <p className="mt-6 font-sans text-lg">
+        Powered by cutting-edge technologies and best practices in front-end
+        development, Dubai Events offers a polished and responsive user
+        experience that demonstrates my skills and attention to detail. From the
+        seamless authentication process to the intuitive event submission form,
+        every feature of this website showcases my ability to create elegant
+        solutions to complex problems.
+      </p>
+      <h3 className=" font-sans font-semibold text-xl mt-6">
+        Join Me on My Journey
+      </h3>
+      <p className="mt-6 font-sans text-lg">
+        Interested in learning more about my skills and experience? Explore
+        Dubai Events today and see firsthand what I can bring to your next
+        front-end development project. Whether you're an employer seeking a
+        talented developer or a fellow enthusiast eager to collaborate, I invite
+        you to join me on this journey of growth and innovation.
+      </p>
+    </div>
+  );
+}
